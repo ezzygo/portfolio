928e6c339c27f1e35cbdc665f802f666008b7726
ezzygo <99530742+ezzygo@users.noreply.github.com>
2022-02-26 01:44:19 +0900

    Add files via upload

---
+++ portfolio/js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+$(document).ready(function () {
+
+    $('.gnb ul li a').on('click', function () {
+        let btnHash = $(this).attr('hash');
+
+        console.log(btnHash);
+    });
+
+});*/
